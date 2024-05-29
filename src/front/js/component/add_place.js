b14a099a57c64c45da8e4eb297feddfff80c26e7
Brittney Hooker <139884664+Bhooker88@users.noreply.github.com>
2024-05-29 23:08:09 +0000

    deploying to heroku

--- src/front/js/component/add_place.js
+++ src/front/js/component/add_place.js
@@ -33,7 +33,7 @@ export const AddPlace = ({typeOfList}) => {
         </div>
         <div className="modal-footer">
           <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" className="btn btn-primary" onClick={() => UpdateList()}>Save Changes</button>
+          <button type="button" className="btn btn-primary" onClick={() => UpdateList()}>Save changes</button>
         </div>
       </div>
     </div>
